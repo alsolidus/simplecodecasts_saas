f55db870df686c6ebea00cdb158cb278e0b2f5c2
William Dubose <wmdubose@gmail.com>
2016-01-26 21:40:57 +0000

    added form edit action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -16,6 +16,11 @@ class ProfilesController < ApplicationController
         end
     end
     
+    def edit
+        @user = User.find( params[:user_id] )
+        @profile = @user.profile
+    end
+    
     private
     def profile_params
         params.require(:profile).permit(:first_name, :last_name, :phone_number, :alt_number, :job_title, :contact_email, :description, :site_code, :hosptial_name)
